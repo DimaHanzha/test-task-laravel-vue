14f721c448cdc93e0ef202e5396c4fd2086d41df
Dima Hanzha <hanzhadima@gmail.com>
2021-04-21 17:33:56 +0300

    Fix query

--- app/Services/HouseService.php
+++ app/Services/HouseService.php
@@ -23,22 +23,22 @@ class HouseService implements HouseInterface
             ->when($data['name'] ?? null, function ($query) use ($data) {
                 return $query->where('name', 'like', '%'.$data['name'].'%');
             })
-            ->when($data['bedrooms'] != null, function ($query) use ($data) {
+            ->when($data['bedrooms'] ?? null, function ($query) use ($data) {
                 return $query->where('bedrooms', $data['bedrooms']);
             })
-            ->when($data['bathrooms'] != null, function ($query) use ($data) {
+            ->when($data['bathrooms'] ?? null, function ($query) use ($data) {
                 return $query->where('bathrooms', $data['bathrooms']);
             })
-            ->when($data['storeys'] != null, function ($query) use ($data) {
+            ->when($data['storeys'] ?? null, function ($query) use ($data) {
                 return $query->where('storeys', $data['storeys']);
             })
-            ->when($data['garages'] != null, function ($query) use ($data) {
+            ->when($data['garages'] ?? null, function ($query) use ($data) {
                 return $query->where('bedrooms', $data['garages']);
             })
-            ->when($data['min_price'] != null, function ($query) use ($data) {
+            ->when($data['min_price'] ?? null, function ($query) use ($data) {
                 return $query->where('price', '>=', $data['min_price']);
             })
-            ->when($data['max_price'] != null, function ($query) use ($data) {
+            ->when($data['max_price'] ?? null, function ($query) use ($data) {
                 return $query->where('price', '<=', $data['max_price']);
             })
             ->paginate(10);
